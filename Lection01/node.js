0e09213789bade669051c2c98eae385c86c1ba7a
Vitaliy <kv1993@me.com>
2024-07-20 23:00:40 +0800

    Added Homework01

--- Lection01/node.js
+++ Lection01/node.js
@@ -1,28 +1,23 @@
 const http = require('http');
 
-let countHome = 0,
-    countAbout = 0;
-
 const server = http.createServer((req, res) => {
     console.log('Запрос получен!');
-    
+
     if (req.url === '/') {
-        countHome++;
 
         res.writeHead(200, {
             "content-type": "text/html; charset=UTF-8"
         });
     
-        res.end(`<a href="/about">ссылка на about</a><br><p>Количество посещений: ${countHome}</p>`);
+        res.end('<h1>Добро пожаловать на мой сайт!</h1>');
 
     } else if (req.url === '/about') {
-        countAbout++;
         
         res.writeHead(200, {
             "content-type": "text/html; charset=UTF-8"
         });
     
-        res.end(`<a href="/">ссылка на главную</a><br><p>Количество посещений: ${countAbout}</p>`);
+        res.end('<h1>about</h1>');
 
     } else {
 
@@ -30,9 +25,11 @@ const server = http.createServer((req, res) => {
             "content-type": "text/html; charset=UTF-8"
         });
     
-        res.end('<h1>404 Not found</h1><br><a href="/">ссылка на главную</a>');
+        res.end('<h1>Not found</h1>');
 
     }
+
+    
 });
 
 const port = 3000;
